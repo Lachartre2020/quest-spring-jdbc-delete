142ac2254b2a370a16b92632cb7b1954a23bd99a
Lachartre2020 <s.lachartre@hotmail.fr>
2021-05-14 21:59:37 +0200

    quete ok

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +20,31 @@ public class SchoolRepository {
 
     public void deleteById(Long id) {
         // TODO: delete a school from the database
+    	Connection connection =null;
+    	PreparedStatement statement = null;
+    	
+    try {
+    	connection = DriverManager.getConnection(
+    			DB_URL, DB_USER, DB_PASSWORD
+    			);
+    	
+    	statement = connection.prepareStatement(
+    		"DELETE FROM school WHERE id=?"
+    			);
+    	
+    	statement.setLong(1, id);
+    	
+    	if(statement.executeUpdate() != 1) {
+    		throw new SQLException("Failed to delete data");
+    	}
+    	    	    	
+    	}catch(SQLException e) {
+    		e.printStackTrace();
+    	}finally {
+    		JdbcUtils.closeStatement(statement);
+    		JdbcUtils.closeConnection(connection);    	
+    	}
+    	    	
     }
 
     public List<School> findAll() {
